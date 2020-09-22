c1b5f042d463703488f72ff601c69a09a161912b
Denis Garley <gar18056@byui.edu>
2020-09-22 08:32:53 -0600

    commit

--- class.py
+++ class.py
@@ -1,19 +1,3 @@
 
 
 #use a dictionary to read in the names 
-
-def studentNames(*students):
-    for std in students:
-        print(std)
-
-students = ["Samuel", "Edward", "Chriss", "LineMan"]
-studentNames(students)
-
-def currentlyRegistered():
-    pass
-
-def studentGradeReport():
-    pass
-
-def main():
-    pass
